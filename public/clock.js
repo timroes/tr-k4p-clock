0469821145602725b6d417d543d80dfc90e55ce1
Tim Roes <mail@timroes.de>
2015-08-13 14:33:26 +0200

    Add Angular controller, make clock working

--- public/clock.js
+++ public/clock.js
@@ -5,6 +5,19 @@ define(function(require) {
 	// Include our custom CSS (LESS also works)
 	require('plugins/tr-k4p-clock/clock.css');
 
+	// Create an Angular module for this plugin
+	var module = require('ui/modules').get('tr-k4p-clock');
+	// Add a controller to this module
+	module.controller('ClockController', function($scope, $timeout) {
+
+		var setTime = function() {
+			$scope.time = Date.now();
+			$timeout(setTime, 1000);
+		};
+		setTime();
+
+	});
+
 	// The provider function must return the visualization
 	function ClockProvider(Private) {
 		// Load TemplateVisType
